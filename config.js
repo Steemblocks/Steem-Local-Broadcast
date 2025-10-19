c468596edcdbd461256c9dcb815ecdecfac33485
Shahriar <51161404+shahriar1933@users.noreply.github.com>
2025-10-19 20:50:08 +0600

    Add Steem API nodes to config
    Added multiple Steem API nodes and current node index.

--- config.js
+++ config.js
@@ -2,5 +2,12 @@
 module.exports = {
     username: 'your-userid', // Replace with your Steem username
     activeKey: 'your-active-key', // Replace with your active private key
+    nodes: [
+        'https://api.steemit.com',
+        'https://api.moecki.online',
+        'https://steemd.steemworld.org',
+        'https://api.steemitdev.com'
+    ],
+    currentNodeIndex: 0, // Index of the currently used node
 };
 
